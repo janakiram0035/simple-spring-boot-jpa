76baaa95f3334bf6f99e1944acd9ddddb48ac280
Janakiram <janakiram0035@gmail.com>
2018-02-20 07:23:39 +0530

    Modified for Syso

--- src/main/java/com/java/appconfig/SpringJdbcConfig.java
+++ src/main/java/com/java/appconfig/SpringJdbcConfig.java
@@ -12,6 +12,7 @@ public class SpringJdbcConfig
 	public static void main(String[] args) 
 	
 	{
+		System.out.println("Hello spring boot ");
 		SpringApplication.run(SpringJdbcConfig.class, args);
 
 	}
